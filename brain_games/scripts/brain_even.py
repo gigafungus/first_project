1a15713525baa32778a3b0eaa9308569d06d8541
fungus <amortis@inbox.ru>
2025-10-26 02:30:39 +0300

    fix: brain-even

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,15 +1,16 @@
 from random import randint
 
+import prompt
+
 from brain_games.cli import welcome_user
 
-import prompt
 
 def is_even(rnd_num):
     return 'yes' if rnd_num % 2 == 0 else 'no'
 
+
 def main():
-    print("Welcome to the Brain Games!")
-    welcome_user()
+    user = welcome_user()
     print('Answer "yes" if the number is even, otherwise answer "no".')
     attempt = 0  # счетчик верных ответов
     max_attempts = 3  # необходимо верных ответов для победы
@@ -23,10 +24,10 @@ def main():
             attempt += 1
             print('Correct!')
             if attempt == max_attempts:
-                return f'Congratulations, {welcome_user()}!'
+                return f'Congratulations, {user}!'
         else:
             print(f"'{answer}' is wrong answer ;(. Correct answer was '{is_even(rnd_num)}'.")
-            return f"Let's try again, {welcome_user()}!"
+            return f"Let's try again, {user}!"
             
 
 
